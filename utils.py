dad577872b10260a0ab7da81ccc3c5ec457a233c
James D. Forrester <jforrester@wikimedia.org>
2015-09-16 21:26:39 +0200

    Support wmf branches like '1.23.0-wmf.6'
    Bug: T107192
    Change-Id: Idae260e43d9246a2698875696067efad18e4ef5a

--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 def wmf_number(branchname):
     """
+    >>> wmf_number('1.27.0-wmf21')
+    12721
+    >>> wmf_number('1.27.0-wmf1')
+    12701
     >>> wmf_number('1.26wmf10')
     12610
     >>> wmf_number('1.25wmf8')
@@ -8,13 +12,22 @@ def wmf_number(branchname):
     False
     >>> wmf_number('phase0')
     False
+    >>> wmf_number('1.27.2-wmf1') # only doing -wmf for .0
+    False
     """
-
     try:
         major, minor = branchname.split('wmf', 1)
     except ValueError:
         return False
+
     major = major.replace('.', '')
+    major = major.replace('-', '')
+    if len(major) == 4:
+        if major[-1] != '0':
+            return False
+        major = major[:-1]
+
+    minor = minor.replace('.', '')
     try:
         int(minor)
     except ValueError:
@@ -23,3 +36,4 @@ def wmf_number(branchname):
         minor = '0' + minor
 
     return int(major + minor)
+
