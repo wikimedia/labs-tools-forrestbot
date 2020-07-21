2d99a8fef16d938292904076d7f759eb6dc5d99a
Kunal Mehta <legoktm@member.fsf.org>
2020-07-21 10:13:12 -0700

    Mail the rest of us when forrestbot breaks
    Per <https://wikitech.wikimedia.org/wiki/Help:Toolforge/Email#Mail_to_a_Tool>,
    this will go to all the maintainers, which is James, me (Legoktm)
    and valhallasw.
    
    Change-Id: I0757ef1c2c3eb1367c36603972642202cea96d55

--- forrestbot.sh
+++ forrestbot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/data/project/forrestbot/venv-py35-stretch/bin/python /data/project/forrestbot/forrestbot/forrestbot.py --logfile /data/project/forrestbot/forrestbot.log || (tail -n 30 /data/project/forrestbot/forrestbot.log | mail -s "Forrestbot broken" valhallasw@arctus.nl)
+/data/project/forrestbot/venv-py35-stretch/bin/python /data/project/forrestbot/forrestbot/forrestbot.py --logfile /data/project/forrestbot/forrestbot.log || (tail -n 30 /data/project/forrestbot/forrestbot.log | mail -s "Forrestbot broken" tools.forrestbot@tools.wmflabs.org)
 
