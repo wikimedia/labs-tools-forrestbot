c47f84041490499389d9b4e2c09438e2f9b5131d
Merlijn van Deen <valhallasw+tools@gmail.com>
2023-08-29 17:28:35 +0000

    Add timeout to forrestbot.sh
    This should resolve some scenarios where the bot doesn't finish.
    
    Change-Id: I536b357f7b16d3b90632c90181efa5852610bbd0

--- forrestbot.sh
+++ forrestbot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/data/project/forrestbot/venv-tf-python39/bin/python /data/project/forrestbot/forrestbot/forrestbot.py --logfile /data/project/forrestbot/forrestbot.log || (/data/project/forrestbot/venv-tf-python39/bin/python /data/project/forrestbot/forrestbot/error_email_k8s.py)
+timeout -k 3700 3600 /data/project/forrestbot/venv-tf-python39/bin/python /data/project/forrestbot/forrestbot/forrestbot.py --logfile /data/project/forrestbot/forrestbot.log || (/data/project/forrestbot/venv-tf-python39/bin/python /data/project/forrestbot/forrestbot/error_email_k8s.py)
 
